74564d62f4a536701da38686e2f4c76301b1c83e
Jolkert <jolkertt@gmail.com>
2022-12-06 20:03:19 -0500

    why did i do that?

--- core/src/jolkert/plabattlesim/data/pokemon/PokemonSpecies.kt
+++ core/src/jolkert/plabattlesim/data/pokemon/PokemonSpecies.kt
@@ -4,9 +4,3 @@ import jolkert.plabattlesim.data.Type
 import jolkert.plabattlesim.data.stats.Stats
 
 data class PokemonSpecies(val name: String, val baseStats: Stats, val types: Pair<Type, Type>)
-{
-	init
-	{
-		Type.None
-	}
-}
